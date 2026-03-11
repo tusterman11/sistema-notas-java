90e135abf7b06a4802d68255a4bb183892cae964
tusterman11 <guilhegabrielferreira@hotmail.com>
2026-03-11 11:46:15 -0300

    git ignore

---
+++ src/App.java
@@ -0,0 +1,25 @@
+public class App {
+    
+    public static void menu() {
+        System.out.println("=== INÍCIO ===\n\n1-Cadastrar Aluno\n2-Cadastrar Nota\n3-Mostrar Tabela de Notas\n4-Mostrar Alunos Aprovados\n5-Mostrar Alunos com maior média\n6-Mostrar média geral\n7-Mostrar disciplina com maior média\n8-Mostrar aprovados em ordem alfabética\n9-Mostrar alunos reprovados em ordem alfabética\n0-Sair");
+    }
+    
+    static String nomes[] = new String[5];
+    static double notas[][] = new double[5][3];
+    
+    
+    
+    
+    
+    
+    
+    public static void main(String[] args) throws Exception {
+        menu();
+   
+   
+   
+   
+   
+   
+    }
+}
